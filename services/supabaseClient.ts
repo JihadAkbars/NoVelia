3c0ee2f4db57dfd4e46fc73080a4075849e8dcbf
MhmdJhdz <jihadakbardoang@gmail.com>
2026-01-01 11:28:47 +0700

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -5,8 +5,8 @@ import { createClient } from '@supabase/supabase-js';
 // 2. Copy "Project URL" and paste it into SUPABASE_URL below
 // 3. Copy "anon public" key and paste it into SUPABASE_KEY below
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL || 'https://YOUR_SUPABASE_PROJECT_URL.supabase.co'; 
-const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY || 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL || 'https://cukaoncgoafbepvsnngi.supabase.co'; 
+const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1a2FvbmNnb2FmYmVwdnNubmdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyMTk1MzIsImV4cCI6MjA4Mjc5NTUzMn0.-wOmTFO7e-Q93XEYLrgDtNoGqWMOrXMS8ahsbU_zAaQ';
 
 let supabaseClient = null;
 
