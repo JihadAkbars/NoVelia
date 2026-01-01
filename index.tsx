0187481b536f7c7cb208979dbe96aa55f38eba0e
MhmdJhdz <jihadakbardoang@gmail.com>
2026-01-01 11:17:09 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -4,18 +4,26 @@ import App from './App';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 
+console.log("Starting NoVelia Application...");
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
+  console.error("Could not find root element to mount to");
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </ThemeProvider>
-  </React.StrictMode>
-);
+try {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <ThemeProvider>
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+  console.log("React mounted successfully.");
+} catch (error) {
+  console.error("Error mounting React app:", error);
+}
